e519f2bfaf7d2a5c221feacebcd0b42e0fa971b8
korsing <joshuanpedia@gmail.com>
2018-07-03 17:05:30 +0000

    MODIFIED [app.py] to check rendering works

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, flash, redirect, url_for, session
-from data import Articles
+# from data import Articles
 
 app = Flask(__name__)
 
-# 홈페이지 (메인화면)
+# Homepage
 @app.route('/')
 # @app.route('/<user>')
 # def index(user=None):
