4d8ada85c60552f60c88d66d14aa60a946795bcc
Korsing <joshuanpedia@gmail.com>
2018-07-04 01:36:48 +0900

    ADDED [profiles.html] - To work on rendering html files.

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session
+from data import Articles
 
 app = Flask(__name__)
 
 # 홈페이지 (메인화면)
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-
-
+@app.route('/<user>')
+def index(user=None):
+    return render_template('index.html', user = user)
 
 @app.route("/profile/<name>")
 def profile(name):
-    return render_template("profile.html")
+    return render_template("profile.html", name = name)
 
 if(__name__ == "__main__"):
     app.run()
