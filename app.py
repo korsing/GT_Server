43479de31a15c3e6d2e7062e9192fa8474368121
Korsing <joshuanpedia@gmail.com>
2018-07-04 01:23:39 +0900

    ADDED [static, template] FOLDER - template folder for HTML files - static folder for CSS, JS files

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# 홈페이지 (메인화면)
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+
+
+@app.route("/profile/<name>")
+def profile(name):
+    return render_template("profile.html")
 
 if(__name__ == "__main__"):
     app.run()
