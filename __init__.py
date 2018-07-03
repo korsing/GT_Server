2fc6004f92f26e1721074dee4f33c765c06d31e1
Korsing <joshuanpedia@gmail.com>
2018-07-04 02:25:09 +0900

    MODIFIED [app.py] - Changed name to __init__.py - Commented some unnecessary lines..

---
+++ __init__.py
@@ -0,0 +1,21 @@
+
+#! /bin/usr/python
+from flask import Flask #, render_template, flash, redirect, url_for, session
+# from data import Articles
+
+app = Flask(__name__)
+
+# Homepage
+@app.route('/')
+def homepage():
+    return "Hello There!"
+
+
+# @app.route('/<user>')
+# def index(user=None):
+#    return render_template('index.html', user = user)
+
+
+
+if(__name__ == "__main__"):
+    app.run()
