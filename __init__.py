3750dd5c2ec70d978b35deb5088ebd8c496c1000
Korsing <joshuanpedia@gmail.com>
2018-07-07 00:06:02 +0900

    html templates organizing.. Test if it works (New ways to path)

--- __init__.py
+++ __init__.py
@@ -5,11 +5,21 @@ from flask import Flask, \
                   flash, \
                   redirect, \
                   url_for, \
-                  session 
+                  session
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length           
 # import MySQLdb # Database Access
 # from datetime import datetime 
 
 app = Flask(__name__)
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('Remember Me')
+
+
 
 # Homepage
 @app.route('/')
@@ -18,23 +28,25 @@ def homepage():
 
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+
+    return render_template("/admin/login.html", form = form)
 
 @app.route("/signup")
 def signup():
-    return render_template("signup.html")
+    return render_template("/admin/signup.html")
 
 @app.route("/leveltest")
 def leveltest():
-    return render_template("leveltest.html")
+    return render_template("/assessments/leveltest.html")
 
 @app.route('/abouttest')
 def aboutleveltest():
-    return render_template("abouttest.html")
+    return render_template("/assessments/abouttest.html")
 
 @app.route('/sensitiveinfo')
 def sensitiveinfo():
-    return render_template("sensitiveinfo.html")
+    return render_template("/privacy/sensitiveinfo.html")
 
 @app.route('/lecture')
 def lecture():
