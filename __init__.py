f557861ca112a1008acdb63cf62b7294fd16eef7
DESKTOP-4SHJTLB\Joshua Y. S. Jung <joshuanpedia@gmail.com>
2018-07-11 05:11:51 +0900

    questions.html path 수정 (4)

--- __init__.py
+++ __init__.py
@@ -61,8 +61,8 @@ def aboutleveltest():
 
 @app.route("/leveltest/Q<qnum>")
 def question(qnum):
-    return "question"+"<qnum>"
-    # return render_template("/assessments/questions/question<qnum>.html") # 테스트 필요
+    return "question"+"qnum"
+    # return render_template("/assessments/questions/question" + str(qnum) + ".html") # 테스트 필요
 
 @app.route('/sensitiveinfo')
 def sensitiveinfo():
