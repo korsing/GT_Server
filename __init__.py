241e21650527e183d6021e55a01bcbab97de5f6f
DESKTOP-4SHJTLB\Joshua Y. S. Jung <joshuanpedia@gmail.com>
2018-07-10 21:49:57 +0900

    오탈자 수정

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 
 #! /bin/usr/python
 from error_code import*
-from flask import Flask, render_template,
+from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 
