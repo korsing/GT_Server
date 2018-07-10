df8dd3e493ea0ba5b3454051d281bc75f9de4404
DESKTOP-4SHJTLB\Joshua Y. S. Jung <joshuanpedia@gmail.com>
2018-07-11 07:07:24 +0900

    갑자기 페이지 전체 다안됨.. import 한거 주석처리해봄

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, IntegerField
 from wtforms.validators import InputRequired
-import MySQLdb
+# import MySQLdb
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "HansClass"
