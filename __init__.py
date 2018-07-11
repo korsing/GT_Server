635e5a23366116fcb4fc8d83384c843ae6fa75af
DESKTOP-P3RJ47C\jkays <joshuanpedia@gmail.com>
2018-07-12 04:38:30 +0900

    혹시 문자열 여러개를 붙이는 과정에서 문제가 발생한건지 확인

--- __init__.py
+++ __init__.py
@@ -42,8 +42,8 @@ def login():
 def signup():
     signup_form = SignupForm()
     if(signup_form.validate_on_submit()):   
-        return "Sign Up Successful!"
-        #return signup_form.name + ' ' + signup_form.userpw + ' ' + signup_form.email
+        # return "Sign Up Successful!"
+        return signup_form.name + '님 환영합니다.'
     return render_template("/admin/signup.html", form = signup_form)
 
 @app.route("/leveltest")
