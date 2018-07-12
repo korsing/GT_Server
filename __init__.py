270c800f661e3c4c0bd4aeb1583339959cef5079
DESKTOP-P3RJ47C\jkays <joshuanpedia@gmail.com>
2018-07-13 04:47:59 +0900

    정제영을 DB에 추가

--- __init__.py
+++ __init__.py
@@ -43,18 +43,17 @@ def connectDB():
 def createSession(username):
     session['user'] = username
 
+
+
 # 홈페이지 # 로그인 없이는 각 버튼 접근 권한 없애야함!
 @app.route('/')
 def homepage():
     if('user' in session):
         userid = session['user']
-        return userid
-        '''
         c, conn = connectDB()
         c.execute("SELECT name FROM USERS WHERE userid = %s", (userid,))
         name = c.fetchone()[0]
         return render_template("index.html", name=name)
-        '''
     else:
         return render_template("index.html", name="NULL")
 
