bc0a04a9c987d353909fbd5f04d39f3a6f15e3f3
DESKTOP-P3RJ47C\jkays <joshuanpedia@gmail.com>
2018-07-12 22:47:48 +0900

    DB 연결 설정할때 param으로 utf8mb4를 던져줌 (기존 utf8)

--- dbtest.py
+++ dbtest.py
@@ -2,12 +2,12 @@
 import MySQLdb
 
 def connection():
-    conn = MySQLdb.connect(host="localhost", user="root", passwd="4swedu@skku", db="GT_DB", charset="utf8")
+    conn = MySQLdb.connect(host="localhost", user="root", passwd="4swedu@skku", db="GT_DB", charset="utf8mb4")
     c = conn.cursor()
     return c, conn
 
 name = "Joshua Jung"
-userid = "korsing3"
+userid = "korsing2"
 userpw = "dbsdud12"
 email = "joshuanpedia@gmail.com"
 phone = "010-2614-5698"
