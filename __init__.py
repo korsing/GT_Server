03a812f316d32ad30ab8449116bcdf519d6922a5
DESKTOP-P3RJ47C\jkays <joshuanpedia@gmail.com>
2018-07-14 07:37:50 +0900

    그냥 변수 전달 테스트..

--- __init__.py
+++ __init__.py
@@ -67,10 +67,7 @@ def homepage():
 
 @app.route('/error')
 def error():
-    if('errmsg' in session): # 현재 무슨 에러가 발생했다면
-        message = session['errmsg'] # 무슨 에러인지 메세지 갖고오고
-    else: # 동작하는지 테스트용도.. 실제로 이 url 치고 들어오는 사람은 없을테니까
-        message = "현재 오류가 없습니다!"
+
     return render_template('/admin/error.html', message = "안녕하세요")
 
 @app.route('/deleteerror')
