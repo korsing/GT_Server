e95e21ee2df15e8edc88d32d57479ba693287443
DESKTOP-P3RJ47C\jkays <joshuanpedia@gmail.com>
2018-07-14 06:53:07 +0900

    로그인 실패 화면 작성 시도 (1)

--- __init__.py
+++ __init__.py
@@ -67,8 +67,11 @@ def homepage():
 
 @app.route('/error')
 def error():
-    message = session['message']
-    deleteMessage
+    if('message' in session): # 현재 무슨 에러가 발생했다면
+        message = session['message'] # 무슨 에러인지 메세지 갖고오고
+        deleteMessage() # 그 메세지는 처리된 것으로 간주하여 삭제
+    else: # 동작하는지 테스트용도.. 실제로 이 url 치고 들어오는 사람은 없을테니까
+        message = "현재 오류가 없습니다!"
     return render_template("/admin/error.html", message=message)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -82,9 +85,10 @@ def login():
         if(userpw == c.fetchone()[0]): # 입력한 비밀번호와 DB상의 비밀번호가 같다면
             createSession(userid) # 로그인이 완료된 상황이니 세션을 생성
             return redirect('/')
-        else:
-
-            return "Login Fail!"
+        else: # 입력한 비밀번호가 DB와 다르다면
+            message = "아이디나 비밀번호가 틀렸습니다."
+            createError(message)
+            return redirect('/error')
     return render_template("/admin/login.html", form=login_form)
 
 @app.route('/onlyformembers')
