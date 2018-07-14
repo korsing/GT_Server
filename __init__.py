564f7b8a052a73ffd5c089d40030082aa8cbc5d6
DESKTOP-P3RJ47C\jkays <joshuanpedia@gmail.com>
2018-07-14 22:52:05 +0900

    signup.html에서 에러 발생 원인 탐색.. (4) 변수를 리턴해서 인식하지 못하는 것인가?

--- __init__.py
+++ __init__.py
@@ -121,7 +121,8 @@ def logout():
 def signup():
     signup_form = SignupForm()
     loginInfo = hashpassword(signup_form.userid.data, signup_form.userpw.data)
-    return "loginInfo.pw_hash"
+    
+    return render_template("lecture.html", message = loginInfo.pw_hash)
     '''
     password = loginInfo.pw_hash
     doubleCheckInfo = hashpassword(signup_form.userid.data, signup_form.pwconfirm.data)
