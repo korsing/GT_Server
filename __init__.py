a7afbd7e87e3ce641e6740adad528d8e457a92a8
DESKTOP-P3RJ47C\jkays <joshuanpedia@gmail.com>
2018-07-16 19:43:46 +0900

    항상 배너가 보이도록 수정

--- __init__.py
+++ __init__.py
@@ -70,11 +70,14 @@ def homepage():
 
 @app.route('/error')
 def error():
-    if('errmsg' in session): # 현재 무슨 에러가 발생했다면
-        message = session['errmsg'] # 무슨 에러인지 메세지 갖고오고
-    else: # 동작하는지 테스트용도.. 실제로 이 url 치고 들어오는 사람은 없을테니까
-        message = ("Congrats! There is no error found.")
-    return render_template("/admin/error.html", message = message[:]) #변수가 안넘어감
+    if('user' not in session):
+        if('errmsg' in session): # 현재 무슨 에러가 발생했다면
+            message = session['errmsg'] # 무슨 에러인지 메세지 갖고오고
+        else: # 동작하는지 테스트용도.. 실제로 이 url 치고 들어오는 사람은 없을테니까
+            message = ("Congrats! There is no error found.")
+        return render_template("/admin/error.html", message = message[:], flag = False) #변수가 안넘어감
+    else:
+        return render_template("/admin/error.html", message = message[:], flag = True)
 
 @app.route('/deleteerror')
 def testtest():
@@ -211,7 +214,7 @@ def questions(qnum):
             conn.commit()
             conn.close()
             return redirect('/leveltest/' + category)
-        return render_template("/assessments/questions/" + category +"/Q" + str(qnum) + ".html", form=form)
+        return render_template("/assessments/questions/" + category +"/Q" + str(qnum) + ".html", form=form, flag = True)
     else:
         return redirect("/onlyformemebers")
 
