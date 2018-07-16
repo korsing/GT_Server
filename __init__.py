969a27d5adf96e5aec286d00dd51419b4f5eeca9
DESKTOP-P3RJ47C\jkays <joshuanpedia@gmail.com>
2018-07-16 19:27:54 +0900

    튜플인지 아닌지

--- __init__.py
+++ __init__.py
@@ -90,7 +90,7 @@ def login():
         c.execute("SELECT userpw FROM USERS WHERE userid = %s", (userid,)) # 아이디를 사용하여 비밀번호를 DB에서 가져옴
         userpw_tuple = c.fetchone()
         if(userpw_tuple==None): # 갖고온게 하나도 없다는 말은 userid가 존재하지 않는다!
-            message = ("아이디가 틀렸습니다.")
+            message = "아이디가 틀렸습니다."
             createError(message)
             return redirect('/error')
         else:
