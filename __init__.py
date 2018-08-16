f6396367eb446a216415ab2cad4d95ce700a745d
DESKTOP-4SHJTLB\Joshua Y. S. Jung <joshuanpedia@gmail.com>
2018-08-17 00:25:24 +0900

    다 풀었다는 검증 논리구조 수정

--- __init__.py
+++ __init__.py
@@ -253,17 +253,17 @@ def leveltest_category(variable):
             if(variable == 'intro'): # 처음 설문조사에 해당
                 query = "SELECT Q10 FROM intro WHERE userid = '" + userid + "';"
                 check = c.execute(query)
-                if(not check): # 10번 문항에 답이 없다면 다 완성한게 아님
+                if(check): # 10번 문항에 답이 없다면 다 완성한게 아님
                     flag = False
             elif(variable == 'thinking'): # 사고력 문제에 해당
                 query = "SELECT Q30 FROM intro WHERE userid = '" + userid + "';"
                 check = c.execute(query)
-                if(not check): # 30번 문항에 답이 없다면 다 완성한게 아님
+                if(check): # 30번 문항에 답이 없다면 다 완성한게 아님
                     flag = False
             else: # 엔트리, 파이썬, C언어에 해당
                 query = "SELECT Q50 FROM " + variable + " WHERE userid = '" + userid + "';"
                 check = c.execute(query)
-                if(not check): # 50번 문항에 답이 없다면 다 완성한게 아님
+                if(check): # 50번 문항에 답이 없다면 다 완성한게 아님
                     flag = False
             if(flag == True): # 위 3가지 경우 모두 안결렸다면 -> 다 푼거임
                 return render_template("/assessments/finished.html")
