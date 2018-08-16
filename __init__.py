6778a987fc8d7a551bcfaffb608d01f08c2a2c43
DESKTOP-4SHJTLB\Joshua Y. S. Jung <joshuanpedia@gmail.com>
2018-08-17 01:08:37 +0900

    설문조사 디비 조회는 잘 됨

--- __init__.py
+++ __init__.py
@@ -177,7 +177,7 @@ def leveltest():
                 query = "SELECT Q10 FROM intro WHERE userid = '" + userid + "';"
                 c.execute(query)
                 check = c.fetchone() # 문제를 풀었으면 안에 뭐라도 있을거고 아니면 None 값
-                return "설문조사 다 풀었는지 조회 중"
+# 여기까지는 잘 됨
                 if(not check): # 10번 문항에 답이 없다면 다 완성한게 아님
                     flag = False
                     return "intro 다 안풀었다!"
