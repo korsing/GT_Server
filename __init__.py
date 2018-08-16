8fa3b9b835a91d0f9b4e8239a83da7382d562ef0
DESKTOP-4SHJTLB\Joshua Y. S. Jung <joshuanpedia@gmail.com>
2018-08-17 00:31:26 +0900

    다 풀었는지 검증 알고리즘 수정

--- __init__.py
+++ __init__.py
@@ -252,21 +252,26 @@ def leveltest_category(variable):
             flag = True # 다 풀었다고 가정
             if(variable == 'intro'): # 처음 설문조사에 해당
                 query = "SELECT Q10 FROM intro WHERE userid = '" + userid + "';"
-                check = c.execute(query)
-                if(check): # 10번 문항에 답이 없다면 다 완성한게 아님
+                c.execute(query)
+                check = c.fetchone()
+                if(not check): # 10번 문항에 답이 없다면 다 완성한게 아님
                     flag = False
             elif(variable == 'thinking'): # 사고력 문제에 해당
                 query = "SELECT Q30 FROM intro WHERE userid = '" + userid + "';"
-                check = c.execute(query)
-                if(check): # 30번 문항에 답이 없다면 다 완성한게 아님
+                c.execute(query)
+                check = c.fetchone()
+                if(not check): # 30번 문항에 답이 없다면 다 완성한게 아님
                     flag = False
             else: # 엔트리, 파이썬, C언어에 해당
                 query = "SELECT Q50 FROM " + variable + " WHERE userid = '" + userid + "';"
-                check = c.execute(query)
-                if(check): # 50번 문항에 답이 없다면 다 완성한게 아님
+                c.execute(query)
+                check = c.fetchone()
+                if(not check): # 50번 문항에 답이 없다면 다 완성한게 아님
                     flag = False
             if(flag == True): # 위 3가지 경우 모두 안결렸다면 -> 다 푼거임
                 return render_template("/assessments/finished.html")
+
+
             query = "SELECT * FROM " + variable + " WHERE userid = '" + userid + "';"
             c.execute(query)
             datalist = c.fetchall()
