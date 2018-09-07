07c4005183400d724b7a8e66817909d08c3a50b4
DESKTOP-4SHJTLB\Joshua Y. S. Jung <joshuanpedia@gmail.com>
2018-09-08 02:46:31 +0900

    phone 변수의 정체를 알아내자..

--- __init__.py
+++ __init__.py
@@ -81,6 +81,7 @@ def login():
         c.execute("SELECT phone FROM USERS WHERE userid = %s", (userid,)) # 아이디를 사용하여 비밀번호를 DB에서 가져옴
         userphone_tuple = c.fetchone()
         userphone = userphone_tuple[9:13]
+        return userphone
         if(userpw_tuple==None): # 갖고온게 하나도 없다는 말은 userid가 존재하지 않는다!
             message = "Your ID or PASSWORD seems to be wrong!"
             createError(message)
