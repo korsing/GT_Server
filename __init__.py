086b6564e06656fbd3007f4a918b622c87677b41
정제영 <pmn03126@gmail.com>
2018-09-11 09:38:28 +0900

    에러 수정

--- __init__.py
+++ __init__.py
@@ -140,7 +140,7 @@ def findid():
         Search_number=['0','1','2','3','4','5','6','7','8','9']
        
         length=len(str(findid_form.gradenumber.data))+len(str(findid_form.classnumber.data))+len(str(findid_form.schoolidnumber.data))
-       return '이거'
+        return '이거'
         schoolid_List=[findid_form.gradenumber.data,findid_form.classnumber.data,findid_form.schoolidnumber.data]
 
         for i in range(len(schoolid_List)):
