78e46a4276a24e058dcbca2e30ac1fd5fcd57665
정제영 <pmn03126@gmail.com>
2018-09-17 18:49:46 +0900

    모두 원상복구하고 디비 다 밀어봄

--- __init__.py
+++ __init__.py
@@ -288,7 +288,7 @@ def signup():
         for category in lists:
             query = "INSERT INTO " + category + "(userid) VALUES ('" + userid +"')"
             c.execute(query)
-            return "이거"
+            
             if (category == "lastquestion"):
                 query = "UPDATE lastquestion set lastnumber  = 0  WHERE userid = '" + userid + "';"
                 c.execute(query)
